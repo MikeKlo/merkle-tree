338f6ab813f80ed83317711924e857fa82f43162
Михаил <mike919@udmlink.ru>
2026-07-03 09:42:49 +0400

    1

--- Tree.js
+++ Tree.js
@@ -17,15 +17,60 @@ class Tree {
     }
 
     addNext( child, parent ){
-        
+        if ( child.data < parent.data ){
+            // add to left
+            if ( parent.left ){
+                this.addNext( child , parent.left )
+                return
+            } else {
+                parent.left = child
+                return
+            }
+        }else{
+            // add to right
+            if ( parent.right ){
+                this.addNext( child , parent.right )
+                return
+            } else {
+                parent.right = child
+                return
+            }
+        }
+
     }
 
     addNode(node){
         // TODO 1 Implement 
+        if (this.root){
+            this.addNext( node, this.root )
+        }else{
+            this.root = node 
+        }
+    }
+
+    hasNext( child_data , parent ){
+        if ( child_data == parent.data ) return true
+        else{
+            if ( child_data < parent.data  ) {
+                // search in left
+                if (parent.left) this.hasNext( child_data , parent.left )
+                    else return false 
+                        
+            }
+            else 
+                // search in right
+                if (parent.right) this.hasNext( child_data , parent.right )
+                    else return false 
+        }
     }
 
     hasNode(data){
         // TODO 2 Implement 
+        if (this.root){
+            return clearthis.hasNext( data , this.root )
+        }else{
+            return false
+        }
     }
 }
 
